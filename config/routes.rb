823964fa88009b070b1457bebc87ac50798d6501
jrollus <rollus.jeremy@gmail.com>
2020-12-19 18:11:51 +0100

    Form auto fill done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
- devise_for :users, controllers: {registrations: 'users/registrations'}
- root to: 'pages#home'
+  # Devise
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+
+  # General routes
+  root to: 'pages#home'
+
+  # API
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :players, only: [ :show ]
+    end
+  end
+
 end
