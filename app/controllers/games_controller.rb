515f4c0566bb94710660b971c2deca44c943dca8
jrollus <rollus.jeremy@gmail.com>
2021-01-22 16:48:33 +0100

    Fix n+1 and GameOrder

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,12 +7,8 @@ class GamesController < ApplicationController
     @min_date = Game.minimum(:date).year
 
     @games = PlayerGamesQuery.new(policy_scope(Game), current_user.player).get_games(@max_date)
-    
-    #@games = structured_output(current_user.player, current_user)
-
-
     @games = GameIndexDecorator.new(@games)
-    
+    @user_player = Player.includes(ranking_histories: :ranking).find(current_user.player.id)
   end
 
   def new
@@ -103,36 +99,4 @@ class GamesController < ApplicationController
     @tournament_dates = query.select_dates(@form.category) unless @form.category.blank?
   end
 
-  def structured_output(player, current_user)
-    structured_output = []
-    selected_player_id = player.id
-    current_user_player_id = current_user.player.id
-    @games.each do |tournament_key, tournament_value| 
-      structured_output << { club: tournament_value.first.tournament.club.name,
-                             category: tournament_value.first.tournament.category.category,
-                             dates: TournamentDecorator.new(tournament_value.first.tournament).tournament_date,
-                             games: []
-                           }
-      tournament_value.sort_by{|tournament| [tournament.date ? 1 : 0, tournament.date] }.reverse.each do |game|
-        game_hash = {}
-        user_score_order = (game.player_id.nil? ? GamePlayerOrderService.maintain?(game, selected_player_id) : (selected_player_id == game.player_id))
-        opponent = game.players.find{|player| player.id != selected_player_id}
-        opponent = PlayerDecorator.new(opponent) if opponent
-        game_hash[:player_name] = PlayerDecorator.new(player).player_description
-        game_hash[:game] = game
-        game_hash[:date] = game.date
-        game_hash[:status] = game.status
-        game_hash[:victory] = (game.game_players.find{|player| player.player_id == selected_player_id}.victory ? 'Victoire' : 'Défaite')
-        if game.game_players.find{|player| player.player_id == current_user_player_id}
-          game_hash[:validated] = game.game_players.find{|player| player.player_id == current_user_player_id}.validated
-        end
-        game_hash[:name] = (opponent ? opponent.full_name : 'N.A.')
-        byebug
-        game_hash[:ranking] = (opponent ? game.game_players.find{|player| player_id.id = selected_player_id}.ranking.name : 'N.A.')
-        game_hash[:score] = GameDecorator.new(game).game_score(selected_player_id)
-        structured_output[-1][:games] << game_hash
-      end
-    end
-    structured_output
-  end
 end
