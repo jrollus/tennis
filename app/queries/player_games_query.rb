9b5ff15577e672b230e38543e1ae02f3ee0233f9
jrollus <rollus.jeremy@gmail.com>
2021-02-21 18:51:19 +0100

    done

--- app/queries/player_games_query.rb
+++ app/queries/player_games_query.rb
@@ -7,12 +7,12 @@ class PlayerGamesQuery
   def get_games(year=nil)
     if year
       query = 'game_players.player_id = ? AND extract(year from games.date) = ?'
-      games = @games.includes(:players, game_players: :ranking, game_sets: :tie_break, tournament: [:club, :category])
+      games = @games.includes(:players, :round, game_players: :ranking, game_sets: :tie_break, tournament: [:club, :category])
                     .joins(:game_players, :tournament).where(query, @player.id, year).order('tournaments.start_date DESC, games.round_id DESC')
                     .group_by(&:tournament_id)
     else
       query = 'game_players.player_id = ?'
-      games = @games.includes(:players, game_players: :ranking, game_sets: :tie_break, tournament: [:club, :category])
+      games = @games.includes(:players, :round, game_players: :ranking, game_sets: :tie_break, tournament: [:club, :category])
                     .joins(:game_players, :tournament).where(query, @player.id).order('tournaments.start_date DESC, games.round_id DESC')
                     .group_by(&:tournament_id)
     end
