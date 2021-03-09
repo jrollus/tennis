c62d37e103b5f291d5f94d46237d49dbcf4be9ea
jrollus <rollus.jeremy@gmail.com>
2021-03-09 23:11:48 +0100

    Interclub done pending check

--- app/queries/player_points_query.rb
+++ app/queries/player_points_query.rb
@@ -4,17 +4,31 @@ class PlayerPointsQuery
       @player = player
     end
   
-    def get_games(start_date=nil, end_date=nil)
+    def get_tournament_games(start_date=nil, end_date=nil)
       if start_date && end_date
-        query = 'game_players.player_id = ? AND (games.date BETWEEN ? AND ?)'
+        query = 'game_players.player_id = ? AND games.interclub_id IS NULL AND (games.date BETWEEN ? AND ?)'
         games = @games.includes(:round, game_players: :ranking, tournament: {category: :category_rounds})
                       .joins(:game_players, :tournament).where(query, @player.id, start_date, end_date).order('tournaments.start_date DESC, games.round_id DESC')
                       .group_by(&:tournament_id)
       else
-        query = 'game_players.player_id = ?'
+        query = 'game_players.player_id = ? AND games.interclub_id IS NULL'
         games = @games.includes(:round, game_players: :ranking, tournament: {category: :category_rounds})
                       .joins(:game_players, :tournament).where(query, @player.id).order('tournaments.start_date DESC, games.round_id DESC')
                       .group_by(&:tournament_id)
+      end
+    end
+
+    def get_interclub_games(start_date=nil, end_date=nil)
+      if start_date && end_date
+        query = 'game_players.player_id = ? AND games.tournament_id IS NULL AND (games.date BETWEEN ? AND ?)'
+        games = @games.includes(interclub: {division: :division_rankings}, game_players: :ranking)
+                      .joins(:game_players, :interclub).where(query, @player.id, start_date, end_date)
+                      .group_by(&:interclub_id)
+      else
+        query = 'game_players.player_id = ? AND games.tournament_id IS NULL'
+        games = @games.includes(interclub: {division: :division_rankings}, game_players: :ranking)
+                      .joins(:game_players, :interclub).where(query, @player.id)
+                      .group_by(&:interclub_id)
       end
     end
   end
