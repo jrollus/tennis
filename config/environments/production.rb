c0f05b5bbc8ce4f3db6f1c6d953b9044a5664d6c
jrollus <rollus.jeremy@gmail.com>
2021-04-06 23:48:43 +0200

    email prod workaround

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
    # ActionMailer
    config.action_mailer.perform_deliveries = true
    config.action_mailer.raise_delivery_errors = true
-   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+   config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
    config.action_mailer.delivery_method = :smtp
    config.action_mailer.smtp_settings = {
      user_name:      ENV['SENDMAIL_USERNAME'],
