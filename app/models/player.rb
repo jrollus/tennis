58c98260a2c32c2ebd187804eddab0ad1cad48f7
jrollus <rollus.jeremy@gmail.com>
2021-06-19 16:16:25 +0200

    done

--- app/models/player.rb
+++ app/models/player.rb
@@ -17,8 +17,8 @@ class Player < ApplicationRecord
   accepts_nested_attributes_for :ranking_histories
 
   # Validations
-  validates :gender, :first_name, :last_name, :dominant_hand, presence: true
-  validates :affiliation_number, :birthdate, presence: true, unless: :user_added?
+  validates :affiliation_number, :gender, :first_name, :last_name, :dominant_hand, presence: true
+  validates :birthdate, presence: true, unless: :user_added?
   validates :affiliation_number, format: { with: /\A\d{7}\z/, message: "le numéro d'affiliation doit être composé de 7 chiffres"}, allow_blank: true
   validate :affiliation_number_check, on: :create
   
@@ -34,17 +34,15 @@ class Player < ApplicationRecord
 
   # Custom Validations
   def affiliation_number_check
-    if self.affiliation_number.present?
-      player = Player.find_by_affiliation_number(self.affiliation_number)
-      # Created through nested attributes
-      if self.user
-        if player
-            errors.add(:affiliation_number, 'Déjà associé à un autre e-mail') if (!player.user_id.nil? && (player.user.email != self.user.email))
-        end
-      # Create as a stand-alone player
-      else
-        errors.add(:affiliation_number, 'Déjà existant') if (player && self.new_record?)
+    player = Player.find_by_affiliation_number(self.affiliation_number)
+    # Created through nested attributes
+    if self.user
+      if player
+          errors.add(:affiliation_number, 'Déjà associé à un autre e-mail') if (!player.user_id.nil? && (player.user.email != self.user.email))
       end
+    # Create as a stand-alone player
+    else
+      errors.add(:affiliation_number, 'Déjà existant') if (player && self.new_record?)
     end
   end
   
